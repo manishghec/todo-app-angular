51febc124172976eac81ca6793333bf81de66698
Manish Sharma <una.manish@gmail.com>
2025-04-09 07:23:25 +0530

    Header and greeting components.

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, signal } from '@angular/core';
+import { GreetingComponent } from "../components/greeting/greeting.component";
+
+@Component({
+  selector: 'app-home',
+  imports: [GreetingComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  homeMessage = signal('Hello World from Home Component');
+
+  keyUpHandler(event: KeyboardEvent): void {
+    // Prevent the default action of the event
+    event.preventDefault();
+    // Log the event to the console
+    console.log(event);
+    // Update the homeMessage signal
+    console.info('Key up event triggered');
+    this.homeMessage.set(`Key up event triggered ${event.key}`);
+  }
+}
