156a7f09c16ff0852874f7170731b90e1cfcf096
Manish Sharma <una.manish@gmail.com>
2025-04-10 00:11:10 +0530

    Completing the todo items component and services update and pipe integration. Almost done what we want to achieve in this.

--- src/app/directives/highlight-completed-todo.directive.ts
+++ src/app/directives/highlight-completed-todo.directive.ts
@@ -1,10 +1,27 @@
-import { Directive } from '@angular/core';
+import { Directive, effect, ElementRef, inject, input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightCompletedTodo]'
 })
 export class HighlightCompletedTodoDirective {
 
+  isCompleted = input(false);
+  e1 = inject(ElementRef);
+
   constructor() { }
 
+  stylesEffect = effect(() => {
+    if(this.isCompleted()) {
+      this.e1.nativeElement.style.textDecoration = 'line-through';
+      this.e1.nativeElement.style.backgroundColor = '#d3f9d8';
+      this.e1.nativeElement.style.color = '#6c757d';
+    } else {
+      this.e1.nativeElement.style.textDecoration = 'none';
+      this.e1.nativeElement.style.backgroundColor = '#fff';
+      this.e1.nativeElement.style.color = '#000';
+    }
+  
+   
+  })
+  
 }
